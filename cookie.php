7304473523025cc9a4ab1da03e2c470c2d8b267c
Your Name <you@example.com>
2023-09-14 11:10:27 +0000

    cookie.phpを書いた

---
+++ cookie.php
@@ -0,0 +1,18 @@
+<?php
+// h()関数の定義
+function h(string $s) : string
+{
+    return htmlspecialchars($s, ENT_QUOTES);
+}
+
+// Cookieから数値を読み込む + 型をintに変換
+$counter = intval($_COOKIE['counter'] ?? 1);
+
+// 出力
+echo "あなたがこのサイトを訪れたのは " , h( strval($counter) ) , "回目ですね。";
+
+// Cookieに設定する値を作成する
+$next_counter = strval($counter + 1);
+
+// Cookieに値を設定する
+setcookie('counter', $next_counter);
